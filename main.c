93289c5cc20b2b3c7147644af7076705f2c0a782
rakazya <rakazya@duck.com>
2026-05-22 21:57:40 +0200

    Prototype for making directories and files in C, added .gitignore

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(){
-    printf("Welcome to T-G-Notes\n");
+    FILE* filePtr = NULL;
+    char notesDirectory[] = "Notes";
+    char command[50];
+
+
+    char text[] = "Amongus \nbe Sus as hell \n" ;
     
+    snprintf(command,sizeof(command),"mkdir %s 2> ''",notesDirectory);
+    int status = system(command);
+
+    printf("Welcome to T-G-Notes\n\n\n");
+
+    if (status == 0 || status == 256){
+        filePtr = fopen("Notes/note1.md","w");
+
+        if (filePtr == NULL)printf("Note not created\n");
+        else{
+            fputs(text,filePtr);
+            fclose(filePtr);
+            printf("Note succesfully created\n");
+        }
+    }
+    else printf("Directory not created \n");
 }
